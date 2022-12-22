2403fa80d13d591895ac85573c79808582ad049b
neasher <neasherahmed75@gmail.com>
2022-12-22 19:44:59 +0600

    layout fix

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,10 +3,10 @@ import img from '../../../src/Asstes/images/dashboard.jpg';
 
 const Dashboard = () => {
     return (
-        <div>
+        <div className='mx-auto'>
             <h2 className='text-4xl font-bold text-primary text-center my-4'>Welcome to My Dashboard</h2>
             <figure>
-                <img className='lg:w-2/4' src={img} alt="dashboard" />
+                <img className='lg:w-2/4 mx-auto' src={img} alt="dashboard" />
             </figure>
         </div>
     );
